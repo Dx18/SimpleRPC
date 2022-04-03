04439baba913e630bf24696edd7e4399a8c893e1
Dx18 <Dx18.E@yandex.ru>
2022-04-03 08:47:28 +0300

    Fixed exiting from REPL

--- examples/repl/main.c
+++ examples/repl/main.c
@@ -26,79 +26,101 @@ void rpc_value_print(struct RPCValue value)
     }
 }
 
+bool parse_query(struct RPCValue** out_values, size_t* out_count)
+{
+    const char* kWhitespace = " \f\n\r\t\v";
+    const size_t kMaxQueryLength = 4096;
+    char query[kMaxQueryLength];
+
+    if (fgets(query, sizeof(query), stdin) == NULL) {
+        return false;
+    }
+
+    char* new_line = strchr(query, '\n');
+    if (new_line != NULL) {
+        *new_line = '\0';
+    }
+
+    size_t count = 0;
+    {
+        char* begin = query;
+        while (1) {
+            begin += strspn(begin, kWhitespace);
+            char* end = begin + strcspn(begin, kWhitespace);
+            if (begin == end) {
+                break;
+            }
+
+            ++count;
+
+            begin = end;
+        }
+    }
+
+    if (count == 0) {
+        *out_values = NULL;
+        *out_count = 0;
+        return true;
+    }
+
+    *out_values = malloc(sizeof(struct RPCValue) * count);
+    *out_count = count;
+
+    {
+        size_t index = 0;
+        char* begin = query;
+        while (1) {
+            begin += strspn(begin, kWhitespace);
+            char* end = begin + strcspn(begin, kWhitespace);
+            if (begin == end) {
+                break;
+            }
+
+            char* int_end;
+            int value_int = strtol(begin, &int_end, 10);
+            if (int_end == end) {
+                (*out_values)[index] = rpc_int(value_int);
+            } else {
+                char* string = malloc(end - begin + 1);
+                memcpy(string, begin, end - begin);
+                string[end - begin] = '\0';
+                (*out_values)[index] = rpc_allocated_string(string);
+            }
+
+            ++index;
+
+            begin = end;
+        }
+    }
+
+    return true;
+}
+
 int main()
 {
     struct RPCClient client;
     rpc_client_init_ipv4(&client, "127.0.0.1", 12000);
 
-    while (1) {
-        const char* kWhitespace = " \f\n\r\t\v";
-        const size_t kMaxQueryLength = 4096;
-        char query[kMaxQueryLength];
-
+    bool should_stop = false;
+    while (!should_stop) {
         printf(">>> ");
         fflush(stdout);
 
-        if (fgets(query, sizeof(query), stdin) == NULL) {
+        struct RPCValue* values;
+        size_t count;
+        if (!parse_query(&values, &count)) {
             break;
-        }
-
-        char* new_line = strchr(query, '\n');
-        if (new_line != NULL) {
-            *new_line = '\0';
-        }
-
-        size_t count = 0;
-        {
-            char* begin = query;
-            while (1) {
-                begin += strspn(begin, kWhitespace);
-                char* end = begin + strcspn(begin, kWhitespace);
-                if (begin == end) {
-                    break;
-                }
-
-                ++count;
-
-                begin = end;
-            }
-        }
-
-        if (count == 0) {
-            continue;
-        }
-
-        struct RPCValue values[count];
-
-        {
-            size_t index = 0;
-            char* begin = query;
-            while (1) {
-                begin += strspn(begin, kWhitespace);
-                char* end = begin + strcspn(begin, kWhitespace);
-                if (begin == end) {
-                    break;
-                }
-
-                char* int_end;
-                int value_int = strtol(begin, &int_end, 10);
-                if (int_end == end) {
-                    values[index] = rpc_int(value_int);
-                } else {
-                    char* string = malloc(end - begin + 1);
-                    memcpy(string, begin, end - begin);
-                    string[end - begin] = '\0';
-                    values[index] = rpc_allocated_string(string);
-                }
-
-                ++index;
-
-                begin = end;
-            }
         }
 
         if (values[0].type != kRPCValueTypeString) {
             printf("Name of procedure must be string\n");
+        } else if (values[0].value_string[0] == '/') {
+            if (strcmp(values[0].value_string, "/exit") == 0) {
+                printf("Exiting...\n");
+                should_stop = true;
+            } else {
+                printf("Unknown built-in command\n");
+            }
         } else {
             struct RPCResult result = rpc_call_array(
                 &client, values[0].value_string, values + 1, count - 1);
@@ -119,6 +141,8 @@ int main()
         for (size_t i = 0; i < count; ++i) {
             rpc_value_destroy(&values[i]);
         }
+
+        free(values);
     }
 
     rpc_client_destroy(&client);
